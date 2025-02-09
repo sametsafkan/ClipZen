b5a4cd2dbb1b8389be46dd53aacb25346cb358c0
samet safkan <sametsafkan@gmail.com>
2025-02-09 16:34:30 +0300

    Initial Commit

---
+++ ClipZen/ClipZenApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClipZenApp.swift
+//  ClipZen
+//
+//  Created by Samet Safkan on 9.02.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClipZenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ClipZenTests/ClipZenTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipZenTests.swift
+//  ClipZenTests
+//
+//  Created by Samet Safkan on 9.02.2025.
+//
+
+import Testing
+@testable import ClipZen
+
+struct ClipZenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
