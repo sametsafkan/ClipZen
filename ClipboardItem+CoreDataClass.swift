0cb4bc5b8db3f0b52febd7edfc70f1197f730613
samet safkan <sametsafkan@gmail.com>
2025-02-10 17:32:15 +0300

    * added multiple files support * fixed text copy issue

---
+++ ClipboardItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ClipboardItem+CoreDataClass.swift
+//  ClipZen
+//
+//  Created by Samet Safkan on 10.02.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class ClipboardItem: NSManagedObject {
+
+}
